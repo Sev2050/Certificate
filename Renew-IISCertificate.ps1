978015749a0b05967ad132d5968f6b1c87be62f1
Sev2050 <84805721+Sev2050@users.noreply.github.com>
2023-08-10 15:12:16 -0400

    Create Renew-IISCertificate.ps1
    Need to think of better function name

---
+++ Renew-IISCertificate.ps1
@@ -0,0 +1,28 @@
+#This function does not renew the certificate but puts it into file so that Task Schedule can renew the certificate
+
+function Renew-IISCertificate {
+    param (
+        [string]$ComputerName,
+        [string]$IISsite
+    )
+
+    $scriptBlock = {
+        param ($thumbprint)
+        
+        Import-Module WebAdministration
+        Remove-Item -Path "C:\temp\CertReq.ps1" -ErrorAction SilentlyContinue
+
+        $OldCert = Get-ChildItem -Path IIS:\SslBindings | Where-Object { $_.Sites.Value -eq $using:IISsite }
+        $OldCertTP = $OldCert.Thumbprint
+
+        New-Item "C:\temp\CertReq.ps1" -ItemType File -Value "certreq -Enroll -machine -q -cert '$OldCertTP' Renew ReuseKeys"
+        Write-Host "Certificate to renew $OldCertTP"
+    }
+
+    Write-Host "Checking for cert bound to IIS: $IISsite on $ComputerName"
+    $result = Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $thumbprint
+    
+}
+
+# Example usage
+#Renew-IISCertificate -ComputerName "ServerName" -IISsite "SiteName" -Thumbprint "ThumbprintValue"
